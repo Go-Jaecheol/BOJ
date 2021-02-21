34188ace965bc6694d2f5313a968992f6d03a47b
Go-Jaecheol <gojaech@naver.com>
2021-02-21 20:41:29 +0900

    Solve BOJ_17141

--- BFS/[17141] 연구소 2/BOJ_17141.py
+++ BFS/[17141] 연구소 2/BOJ_17141.py
@@ -1,13 +1,16 @@
 from collections import deque
+from itertools import combinations
+
 N, M = map(int, input().split())
 lab = [[int(x) for x in input().split()]for y in range(N)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-result = 50*50+1
+MAX_SIZE = 50*50+1
+result = MAX_SIZE
 
-def spreadVirus():
+def spreadVirus(copy_lab):
     global result
-    copy_lab = [lab[i][:] for i in range(N)]
+    
     time = [[0 for x in range(N)]for y in range(N)]
     que = deque()
 
@@ -24,21 +27,31 @@ def spreadVirus():
                 if copy_lab[next_y][next_x] == 0 or copy_lab[next_y][next_x] == 2:
                     copy_lab[next_y][next_x] = -1
                     time[next_y][next_x] = time[y][x] + 1
-                    que.append((next_x, next_y))
+                    que.append((next_y, next_x))
 
+    for i in range(N):
+        for j in range(N):
+            if copy_lab[i][j] == 0 or copy_lab[i][j] == 2:
+                return
+    
     max_time = max(map(max, time))
     result = min(result, max_time)
 
 def putVirus(count):
-    if count == M:
-        spreadVirus()
-        return
+    virus = []
     for i in range(N):
         for j in range(N):
             if lab[i][j] == 2:
-                lab[i][j] = -1
-                putVirus(count+1)
-                lab[i][j] = 0
+                virus.append((i, j))
+    comb = list(combinations(virus, M))
+    for c in comb:
+        copy_lab = [lab[i][:] for i in range(N)]
+        for i in range(M):
+            copy_lab[c[i][0]][c[i][1]] = -1
+        spreadVirus(copy_lab)
 
 putVirus(0)
-print(result)
+if result == MAX_SIZE:
+    print(-1)
+else:
+    print(result)
